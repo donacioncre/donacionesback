c341eb85f3866fe7e04e07a4777590f313958b25
mkc.r14l93 <mkc.r14l93@gmail.com>
2022-11-08 17:42:28 -0500

    donation

--- donaciones-api/app/Http/Controllers/Api/DonationController.php
+++ donaciones-api/app/Http/Controllers/Api/DonationController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\DonationRepository;
+use Exception;
 use Illuminate\Http\Request;
 
 class DonationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
+    
+    private $successStatus = 200;
+    private $errorStatus = 500;
+    protected $donation;
+
+    public function __construct(DonationRepository $donation)
+    {
+        $this->donation = $donation;
+    }
+
     public function index()
     {
-        //
+        try {
+            $data = $this->donation->list();
+            return response()->json(['status' => true, 'data' => $data]);
+        } catch (Exception $ex) {
+            dd($ex);
+            return response()->json(['status' => false, 'error' => 'Algo a sucedido por favor intente después de unos minutos', 'message' => $ex->getMessage()], $this->errorStatus);
+        }
     }
 
     /**
