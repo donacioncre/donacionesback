663c2e3bef178851e2ea0d2265735530a7efe103
mkc.r14l93 <mkc.r14l93@gmail.com>
2023-01-05 15:33:18 -0500

    show schedule

--- donaciones-api/app/Repositories/NotificationRepository.php
+++ donaciones-api/app/Repositories/NotificationRepository.php
@@ -54,7 +54,7 @@ class NotificationRepository
                 "priority" => "high",
             ]
         ];
-        dd($data);
+
         $encodedData = json_encode($data);
 
         $headers = [
@@ -64,16 +64,24 @@ class NotificationRepository
 
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        // Execute post
+        $result = curl_exec($ch);
 
-        $response = curl_exec($ch);
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }
+        // Close connection
+        curl_close($ch);
 
-        dd($response);
         //return $result;
     }
 
