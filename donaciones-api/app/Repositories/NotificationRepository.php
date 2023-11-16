aec03f8b65a39bfb0f9afd49aee06af95b524162
mauro <mkc.r14l93@gmail.com>
2023-11-15 22:06:50 -0500

    notificacion

--- donaciones-api/app/Repositories/NotificationRepository.php
+++ donaciones-api/app/Repositories/NotificationRepository.php
@@ -51,10 +51,10 @@ class NotificationRepository
             "registration_ids" =>$firebaseToken,
             "notification" => [
                 "title" => "Convocatoria para donar sangre",
-                "body" => ['notification'=>$notification,  'data' => $dataNotif] ,
+                "body" => $notification,
                 "content_available" => true,
                 "priority" => "high",
-
+                //'data' => $dataNotif
             ]
 
         ];
@@ -71,9 +71,10 @@ class NotificationRepository
             "registration_ids" =>$firebaseToken,
             "notification" => [
                 "title" => "Convocatoria para donar sangre",
-                "body" => ['notification'=>$notification,  'data' => $dataNotif] ,
+                "body" => $notification,
                 "content_available" => true,
                 "priority" => "high",
+                //'data' => $dataNotif
             ],
 
         ];
